6a6f317c091f6d2b3cea8c84629aef22610bad18
rmohanmd <rmohan.md@gmail.com>
2022-08-25 15:13:50 -0400

    Added obj.reduce method to solve findTheOldest problem)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,18 +1,29 @@
-const findTheOldest = function (people) {
-  const oldest = people.sort((a, b) => {
-    if (!("yearOfDeath" in a)) a.yearOfDeath = new Date().getFullYear();
-    if (!("yearOfDeath" in b)) b.yearOfDeath = new Date().getFullYear();
-    const prevGuy = a.yearOfDeath - a.yearOfBirth;
-    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+// const findTheOldest = function (people) {
+//   const oldest = people.sort((a, b) => {
+//     if (!("yearOfDeath" in a)) a.yearOfDeath = new Date().getFullYear();
+//     if (!("yearOfDeath" in b)) b.yearOfDeath = new Date().getFullYear();
+//     const prevGuy = a.yearOfDeath - a.yearOfBirth;
+//     const nextGuy = b.yearOfDeath - b.yearOfBirth;
 
-    return prevGuy > nextGuy ? -1 : 1;
+//     return prevGuy > nextGuy ? -1 : 1;
+//   });
+//   return oldest[0];
+// };
+
+// use reduce
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, nextPerson) => {
+    const oldAge = getAges(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    const nextAge = getAges(nextPerson.yearOfBirth, nextPerson.yearOfDeath);
+
+    return oldAge < nextAge ? nextPerson : oldestPerson;
   });
-  return oldest[0];
 };
 
-// const findTheOldest = function () {
-
-// };
+function getAges(birth, death) {
+  if (!death) death = new Date().getFullYear();
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
