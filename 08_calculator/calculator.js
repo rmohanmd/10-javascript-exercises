59de6ebbbe1f7653abf2901afb6e5d3a9c0a2e13
Rahul Mohan <rmohan.md@gmail.com>
2022-08-25 03:43:37 -0400

    Reworked logic for sum()

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,17 +6,11 @@ const subtract = function (num1, num2) {
   return num1 - num2;
 };
 
-// const sum = function (givenArray) {
-//   let total = 0;
-//   for (i = 0; i < givenArray.length; i++) {
-//     total = total + givenArray[i];
-//   }
-//   return total;
-// };
-
-const sum = (givenArray.reduce(runningTotal, numbs)=>{
-return runningTotal + numbs;}, 0);
-
+const sum = (givenArray) => {
+  return givenArray.reduce((tots, numb) => {
+    return (tots += numb);
+  }, 0);
+};
 
 const multiply = function (givenArray) {
   let total = 1;
