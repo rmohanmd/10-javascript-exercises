d03b91ada6f4bedf3de6bd330482f1d2cb685b9c
rmohanmd <rmohan.md@gmail.com>
2022-08-25 14:54:20 -0400

    solved getThe Titles and finTheOldest with my logic

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.sort((a, b) => {
+    if (!("yearOfDeath" in a)) a.yearOfDeath = new Date().getFullYear();
+    if (!("yearOfDeath" in b)) b.yearOfDeath = new Date().getFullYear();
+    const prevGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
 
+    return prevGuy > nextGuy ? -1 : 1;
+  });
+  return oldest[0];
 };
+
+// const findTheOldest = function () {
+
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
